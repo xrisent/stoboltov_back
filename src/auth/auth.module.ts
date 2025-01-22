d45f8159d5420121d3d4adc776b630134daced8a
xrisent <ormonov31261@gmail.com>
2025-01-22 12:21:06 +0600

    small changes in jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module'; 
 import { JwtStrategy } from './jwt.strategy';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [

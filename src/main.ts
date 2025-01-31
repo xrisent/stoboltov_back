e601533e1fe2bfce2d898d8b4b437953db100ffb
xrisent <ormonov31261@gmail.com>
2025-02-01 02:36:28 +0600

    docker--daw

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule); // Обычный HTTP
+  const app = await NestFactory.create(AppModule); // HTTP-сервер
   const httpsOptions = {
-    key: fs.readFileSync('/etc/ssl/private/selfsigned.key'),
-    cert: fs.readFileSync('/etc/ssl/certs/selfsigned.crt'),
+    key: fs.readFileSync('ssl/selfsigned.key'),
+    cert: fs.readFileSync('ssl/selfsigned.crt'),
   };
-  const httpsApp = await NestFactory.create(AppModule, { httpsOptions }); // HTTPS
+  const httpsApp = await NestFactory.create(AppModule, { httpsOptions }); // HTTPS-сервер
 
   app.enableCors({ origin: '*', credentials: true });
   httpsApp.enableCors({ origin: '*', credentials: true });
 
-  const config = new DocumentBuilder().setTitle('API').addBearerAuth().build();
+  const config = new DocumentBuilder()
+    .setTitle('Stoboltov API')
+    .setDescription('Документация API Stoboltov')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3002, '0.0.0.0'); // HTTP
   await httpsApp.listen(3443, '0.0.0.0'); // HTTPS
 }
+
 bootstrap();
